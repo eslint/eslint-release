1ebdc4eb96833cc1cf7a08a55f4434ba56af59cd
Brandon Mills <mills.brandont@gmail.com>
2016-02-14 21:47:40 -0500

    Update: Exclude reverted commits (fixes #3)
    When generating changelogs or calculating new versions, reverted commits
    are ignored.

--- tests/lib/release-ops.js
+++ tests/lib/release-ops.js
@@ -49,9 +49,9 @@ describe("ReleaseOps", function() {
 
         it("should create a patch release when only bug fixes are present", function() {
             var logs = [
-                    "Fix: Something",
-                    "Docs: Something else",
-                    "Fix: Something else"
+                    "* abcdef0 Fix: Something (Foo Bar)",
+                    "* 1234567 Docs: Something else (foobar)",
+                    "* a1b2c3d Fix: Something else (Foo B. Baz)"
                 ],
                 releaseInfo = ReleaseOps.calculateReleaseFromGitLogs("1.0.0", logs);
 
@@ -60,11 +60,11 @@ describe("ReleaseOps", function() {
                 type: "patch",
                 changelog: {
                     fix: [
-                        "Fix: Something",
-                        "Fix: Something else"
+                        "* abcdef0 Fix: Something (Foo Bar)",
+                        "* a1b2c3d Fix: Something else (Foo B. Baz)"
                     ],
                     docs: [
-                        "Docs: Something else"
+                        "* 1234567 Docs: Something else (foobar)"
                     ]
                 },
                 rawChangelog: logs.join("\n")
@@ -73,10 +73,10 @@ describe("ReleaseOps", function() {
 
         it("should create a minor release when enhancements are present", function() {
             var logs = [
-                    "Fix: Something",
-                    "Docs: Something else",
-                    "Fix: Something else",
-                    "Update: Foo"
+                    "* f9e8d7c Fix: Something (Author Name)",
+                    "* facecab Docs: Something else (authorname)",
+                    "* dec0ded Fix: Something else (First Last)",
+                    "* facade5 Update: Foo (dotstar)"
                 ],
                 releaseInfo = ReleaseOps.calculateReleaseFromGitLogs("1.0.0", logs);
 
@@ -85,14 +85,14 @@ describe("ReleaseOps", function() {
                 type: "minor",
                 changelog: {
                     fix: [
-                        "Fix: Something",
-                        "Fix: Something else"
+                        "* f9e8d7c Fix: Something (Author Name)",
+                        "* dec0ded Fix: Something else (First Last)"
                     ],
                     docs: [
-                        "Docs: Something else"
+                        "* facecab Docs: Something else (authorname)"
                     ],
                     update: [
-                        "Update: Foo"
+                        "* facade5 Update: Foo (dotstar)"
                     ]
                 },
                 rawChangelog: logs.join("\n")
@@ -101,11 +101,11 @@ describe("ReleaseOps", function() {
 
         it("should create a major release when breaking changes are present", function() {
             var logs = [
-                    "Fix: Something",
-                    "Docs: Something else",
-                    "Fix: Something else",
-                    "Update: Foo",
-                    "Breaking: Whatever"
+                    "* abcd123 Fix: Something (githubhandle)",
+                    "* a1b2c3d Docs: Something else (Committer Name)",
+                    "* 321dcba Fix: Something else (Abc D. Efg)",
+                    "* 9876543 Update: Foo (Tina Tester)",
+                    "* 1234567 Breaking: Whatever (Toby Testing)"
                 ],
                 releaseInfo = ReleaseOps.calculateReleaseFromGitLogs("1.0.0", logs);
 
@@ -114,30 +114,69 @@ describe("ReleaseOps", function() {
                 type: "major",
                 changelog: {
                     fix: [
-                        "Fix: Something",
-                        "Fix: Something else"
+                        "* abcd123 Fix: Something (githubhandle)",
+                        "* 321dcba Fix: Something else (Abc D. Efg)"
                     ],
                     docs: [
-                        "Docs: Something else"
+                        "* a1b2c3d Docs: Something else (Committer Name)"
                     ],
                     update: [
-                        "Update: Foo"
+                        "* 9876543 Update: Foo (Tina Tester)"
                     ],
                     breaking: [
-                        "Breaking: Whatever"
+                        "* 1234567 Breaking: Whatever (Toby Testing)"
                     ]
                 },
                 rawChangelog: logs.join("\n")
             });
         });
 
+        it("should disregard reverted commits", function() {
+            var logs = [
+                    "* abcd123 Docs: Update something in the docs (githubhandle)",
+                    "This is the body.",
+                    "It has multiple lines.",
+                    "* a1b2c3d Revert \"Breaking: A breaking change (fixes #1234)\" (Committer Name)",
+                    "This reverts commit abcdef010c481d5da8d2d9b5ef74945e6566166c.",
+                    "This explains why.",
+                    "* 321dcba Fix: Fix a bug (fixes #4321) (Abc D. Efg)",
+                    "Describe the bug.",
+                    "* 9876543 Revert \"New: Add cool new feature (fixes #42)\" (Tina Tester)",
+                    "This reverts commit 123456710c481d5da8d2d9b5ef74945e6566166c.",
+                    "* abcdef0 Breaking: A breaking change (fixes #1234) (Cool Committer)",
+                    "* 1234abc Revert \"New: From a previous release (fixes #1234)\" (Foo Bar)",
+                    "This reverts commit 0123456789abcdeffedcba9876543210a1b2c3d4.",
+                    "* 1234567 New: Add cool new feature (fixes #42) (Toby Testing)",
+                    "Something about this change."
+                ],
+                releaseInfo = ReleaseOps.calculateReleaseFromGitLogs("1.0.0", logs);
+
+            assert.deepEqual(releaseInfo, {
+                version: "1.0.1",
+                type: "patch",
+                changelog: {
+                    docs: [
+                        "* abcd123 Docs: Update something in the docs (githubhandle)"
+                    ],
+                    fix: [
+                        "* 321dcba Fix: Fix a bug (fixes #4321) (Abc D. Efg)"
+                    ]
+                },
+                rawChangelog: [
+                    "* abcd123 Docs: Update something in the docs (githubhandle)",
+                    "* 321dcba Fix: Fix a bug (fixes #4321) (Abc D. Efg)",
+                    "* 1234abc Revert \"New: From a previous release (fixes #1234)\" (Foo Bar)"
+                ].join("\n")
+            });
+        });
+
         it("should create a prerelease when passed a prereleaseId", function() {
             var logs = [
-                    "Fix: Something",
-                    "Docs: Something else",
-                    "Fix: Something else",
-                    "Update: Foo",
-                    "Breaking: Whatever"
+                    "* abcd123 Fix: Something (githubhandle)",
+                    "* a1b2c3d Docs: Something else (Committer Name)",
+                    "* 321dcba Fix: Something else (Abc D. Efg)",
+                    "* 9876543 Update: Foo (Tina Tester)",
+                    "* 1234567 Breaking: Whatever (Cool Committer)"
                 ],
                 releaseInfo = ReleaseOps.calculateReleaseFromGitLogs("1.0.0", logs, "alpha");
 
@@ -146,17 +185,17 @@ describe("ReleaseOps", function() {
                 type: "major",
                 changelog: {
                     fix: [
-                        "Fix: Something",
-                        "Fix: Something else"
+                        "* abcd123 Fix: Something (githubhandle)",
+                        "* 321dcba Fix: Something else (Abc D. Efg)"
                     ],
                     docs: [
-                        "Docs: Something else"
+                        "* a1b2c3d Docs: Something else (Committer Name)"
                     ],
                     update: [
-                        "Update: Foo"
+                        "* 9876543 Update: Foo (Tina Tester)"
                     ],
                     breaking: [
-                        "Breaking: Whatever"
+                        "* 1234567 Breaking: Whatever (Cool Committer)"
                     ]
                 },
                 rawChangelog: logs.join("\n")
@@ -165,9 +204,9 @@ describe("ReleaseOps", function() {
 
         it("should create a prerelease when passed a prereleaseId and prerelease version", function() {
             var logs = [
-                    "Fix: Something",
-                    "Docs: Something else",
-                    "Fix: Something else"
+                    "* abcd123 Fix: Something (githubhandle)",
+                    "* a1b2c3d Docs: Something else (Committer Name)",
+                    "* 321dcba Fix: Something else (Abc D. Efg)"
                 ],
                 releaseInfo = ReleaseOps.calculateReleaseFromGitLogs("2.0.0-alpha.0", logs, "alpha");
 
@@ -176,11 +215,11 @@ describe("ReleaseOps", function() {
                 type: "patch",
                 changelog: {
                     fix: [
-                        "Fix: Something",
-                        "Fix: Something else"
+                        "* abcd123 Fix: Something (githubhandle)",
+                        "* 321dcba Fix: Something else (Abc D. Efg)"
                     ],
                     docs: [
-                        "Docs: Something else"
+                        "* a1b2c3d Docs: Something else (Committer Name)"
                     ]
                 },
                 rawChangelog: logs.join("\n")
