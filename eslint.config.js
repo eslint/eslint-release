9d6b13e4c04e796ae5552cca4dfab401f0fca842
루밀LuMir <rpfos@naver.com>
2025-09-17 23:38:18 +0900

    wip

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const { defineConfig } = require("eslint/config");
+const eslintConfigESLint = require("eslint-config-eslint/cjs");
+const globals = require("globals");
+
+module.exports = defineConfig([
+    eslintConfigESLint,
+    {
+        files: ["lib/**/*.js", "bin/**/*.js"],
+        rules: {
+            "no-console": "off",
+            "require-unicode-regexp": "off",
+            "n/no-process-exit": "off",
+            "n/no-unsupported-features/es-syntax": "off",
+        }
+    },
+    {
+        files: ["tests/**/*.js"],
+        languageOptions: {
+            globals: {
+                ...globals.mocha
+            }
+        }
+    }
+]);
